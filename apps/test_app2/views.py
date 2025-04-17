cf58f7b59f5e07fbdb0ea460b53f8b35aeb86b57
Mshkss <tolya200059@gmail.com>
2025-04-18 01:11:44 +0700

    123

--- apps/test_app2/views.py
+++ apps/test_app2/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import csv
+from io import StringIO
 
 @csrf_exempt  # Отключаем CSRF для упрощения (в продакшене лучше настроить токены)
 def double_number(request):
@@ -8,10 +10,26 @@ def double_number(request):
         try:
             # Получаем данные из тела запроса
             data = json.loads(request.body)
-            number = data.get("number", 0)
+            fullName = data.get("fullName", 0)
+            email = data.get("email",0)
+            phone = data.get("phone",0)
+            additionalInfo = data.get("additionalInfo",0)
 
-            # Умножаем число на 2
-            result = number * 2
+            # Создаем CSV в памяти
+            csv_file = StringIO()
+            csv_writer = csv.writer(csv_file)
+            
+            # Записываем заголовки и данные
+            csv_writer.writerow(["Full Name", "Email", "Phone", "Additional Info"])
+            csv_writer.writerow([fullName, email, phone, additionalInfo])
+            
+            # Сохраняем CSV в файл
+            with open("output.csv", "w", encoding="utf-8") as f:
+                f.write(csv_file.getvalue())
+            
+            result = "CSV file created successfully"
+
+
             # Возвращаем результат
             return JsonResponse({"result": result}, status=200)
         except Exception as e:
